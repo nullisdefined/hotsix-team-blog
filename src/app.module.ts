2a3012e2bbec12e798b77eecc787ac00c4ad8976
Hun <dudgns9194@gmail.com>
2024-06-21 19:43:38 +0900

    초기세팅 확인

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
-import { BoardsModule } from './boards/boards.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './configs/typeorm.config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(typeORMConfig),
-    BoardsModule
-  ],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), UsersModule],
+  controllers: [AppController],
+  providers: [AppService]
 })
 export class AppModule {}
